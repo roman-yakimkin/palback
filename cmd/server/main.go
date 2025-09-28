519a6d1810edf738215f0c0d3cd71a17c02dc8c5
Roman Yakimkin <r.yakimkin@vkteam.ru>
2025-09-28 12:32:02 +0300

    002: Добавить возможность выполнять миграции

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
+	db, err := sql.Open("postgres", "host=localhost port=5444 user=paldev password=paldev dbname=paldev sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("A new version of the pilgrimage project palomniki.su will be there")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module palback
 
-go 1.24
+go 1.25
+
+require github.com/lib/pq v1.10.9 // indirect
